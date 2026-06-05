e841e6e6c13845022d58e2473c824a6b4d285582
Pawel Obrzut <pawel.r.obrzut@gmail.com>
2026-06-05 20:27:21 +0200

    feat: set connection to DB

---
+++ server/Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using server.Models;
+
+namespace server.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.Property(u => u.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            entity.Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            entity.Property(u => u.PasswordHash)
+                .IsRequired();
+
+            entity.HasIndex(u => u.Email)
+                .IsUnique();
+        });
+    }
+}
